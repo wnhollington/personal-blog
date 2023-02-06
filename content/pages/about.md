130a3fc162b17419c3f3a03cdd1e07750eedbcbd
Neal Hollington <wnhollington@outlook.com>
2023-02-06 11:10:51 -0700

    include Tailwindcss

---
+++ content/pages/about.md
@@ -0,0 +1,7 @@
+---
+title: About
+date: "2015-05-01T22:12:03.284Z"
+description: "About Me"
+---
+
+This is the about me page.
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,10 @@
 // custom typefaces
 import "@fontsource/montserrat/variable.css"
 import "@fontsource/merriweather"
+s
 // normalize CSS across browsers
-import "./src/normalize.css"
-import "./src/style.css"
+import "./src/styles/normalize.css"
+import "./src/styles/style.css"
 import "./src/styles/global.css"
 
 // Highlighting for code blocks
