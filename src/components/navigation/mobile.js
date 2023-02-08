e0b8d1c9693e53528fd570628008b5815a122da1
Neal Hollington <wnhollington@outlook.com>
2023-02-08 06:28:17 -0700

    consolidate navigation menus

--- src/components/navigation/mobile.js
+++ src/components/navigation/mobile.js
@@ -1,16 +1,13 @@
 import * as React from "react"
-import { useSiteMetadata } from '../../hooks/use-site-metadata'
-import { Link } from "gatsby"
-import Bio from "../bio";
-import {AiFillLinkedin, AiFillTwitterSquare } from "react-icons/ai"
-import {GrGatsbyjs, GrClose} from "react-icons/gr"
+
+import {GrClose} from "react-icons/gr"
 import {RxHamburgerMenu} from "react-icons/rx"
 import Drawer from 'react-modern-drawer'
 import 'react-modern-drawer/dist/index.css'
+import DesktopNav from "./desktop";
 
 
 const MobileNav = () => {
-  const {social} = useSiteMetadata();
   const [isOpen, setIsOpen] = React.useState(false)
   const toggleDrawer = () => {
       setIsOpen((prevState) => !prevState)
@@ -25,35 +22,7 @@ const MobileNav = () => {
               onClose={toggleDrawer}
               direction='left'
           >
-              
-            <Bio/>
-
-            <div className="flex flex-col py-4 items-center">
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/">Articles</Link>
-              <Link to="/contact">Contact</Link>
-              <Link to="/schedule-consultation">Schedule Consultation</Link>
-            </div>
-
-            <div className="py-4 flex flex-col items-center">
-              <h2>Follow Me</h2>
-              <div className="flex flex-row">
-                <a href={social.linkedin} target="_blank"><AiFillLinkedin size={28}/></a>
-                <a href={social.twitter} target="_blank"><AiFillTwitterSquare size={28}/></a>
-              </div>
-            </div>
-
-            <div className="absolute bottom-0 text-center mx-4">
-
-            <Link to="/privacy-policy" className="m-1">Privacy Policy</Link>
-            <Link to="/disclaimer" className="m-1">Disclaimer</Link>
-
-            <div>
-              © {new Date().getFullYear()}, <a href="https://www.gatsbyjs.com" target="_blank">Built with <GrGatsbyjs className="inline"/></a>
-            </div>
-
-            </div>
+            <DesktopNav />
           </Drawer>
           
           <button onClick={toggleDrawer}>
