c5fce8ed5c8194db9e3f76f3f17879e052a8a25a
Neal Hollington <wnhollington@outlook.com>
2023-02-25 10:55:46 -0700

    add #topics to bio

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -17,6 +17,7 @@ export const useSiteMetadata = () => {
                             linkedin
                             twitter
                         }
+                        talksAbout
                     }
                 }
             }
